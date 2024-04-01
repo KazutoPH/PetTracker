714dac8ac1bb17034b1881c6e3107329bcce334a
KazutoPH <raymartgomez310@gmail.com>
2024-04-01 17:05:20 +0800

    register route redirect

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,7 +1,15 @@
 import Login from "@/components/forms/Login";
 import React from "react";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-const Page = () => {
+const Page = async () => {
+  const session = await getServerSession();
+
+  if (session) {
+    redirect(`/profile`);
+  }
+
   return (
     <main className=" flex flex-1 justify-center items-center h-screen padding-container">
       <Login />
